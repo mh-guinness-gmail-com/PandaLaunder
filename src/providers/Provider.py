49d425a64933e1c5efde0b2863359d1fc078d191
Daniel Hermon <danielhermonsync@gmail.com>
2020-04-05 13:53:57 +0300

    fix lint

--- src/providers/Provider.py
+++ src/providers/Provider.py
@@ -5,13 +5,8 @@ from typing import List, Tuple
 class Provider(ABC):
     def __init__(self):
         """
-<<<<<<< HEAD
         Interface for a Provider.
         A Provider is a class which is responsible for resolving download links,
-=======
-        Interface for a Provider. 
-        A Provider is a class which is responsible for resolving download links
->>>>>>> fb2cc0031ad2224836bd957c20938422420e13e9
         for its respective product, for example an npm-package(product) and npm (Provider)
         pip-package(product) and pip (provider).
         """
