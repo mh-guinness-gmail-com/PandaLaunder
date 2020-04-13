fcfe0fe125a5c21de3434dbbe5db879092950b00
nir levy <nirnir93@gmail.com>
2020-04-13 13:00:52 +0300

    refactor: refactored all code
    Modified cmd flags to be flags and not inputs
    Moved multi process to multi thread with task queue
    
    closes #9
    closes #3

---
+++ src/command_line.py
@@ -0,0 +1,27 @@
+import argparse
+from str2bool import str2bool
+
+
+from src.providers import providers
+
+
+__parser = argparse.ArgumentParser()
+
+__parser.add_argument('--concurrency-per-core', '-c', type=int, default=2, dest='concurrency',
+                      help='Number of workers = concurrency * CPU_CORES_COUNT')
+__parser.add_argument('--temp-dir', '-t', type=str, default='./tmp',
+                      help='Base directory to place all downloaded files before bundling')
+__parser.add_argument('--output-dir', '-o', type=str, default='./out',
+                      help='Base directory to place bundled zip file in')
+__parser.add_argument('--input-dir', '-i', type=str, default='./in',
+                      help='Base directory where list files are located')
+__parser.add_argument('--strict-ssl', type=str2bool, default=True,
+                      help='False disables ssl certificate checks')
+
+for provider in providers:
+    __parser.add_argument('--{0}'.format(provider['name']),
+                          action='store_true',
+                          default=False,
+                          help='specify if should download {0}'.format(provider['products']))
+
+args = __parser.parse_args()
---
+++ src/http_util.py
@@ -0,0 +1,12 @@
+from src.providers import Provider
+
+
+def validate_http_status_code(status_code: int, provider: Provider, product_name: str, product_version: str) -> None:
+    message = None
+    if status_code == 404:
+        message = 'Product not found {0}:{1}'
+    elif status_code > 399:
+        message = 'Unknown HTTP error occurred {0}:{1}'
+
+    if message:
+        raise Exception(message.format(product_name, product_version))
