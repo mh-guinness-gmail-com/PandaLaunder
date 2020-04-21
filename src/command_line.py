3d6802672fd3242e392f3c0e593c1c72536e9417
Niro Levy <nirnir93@gmail.com>
2020-04-21 14:40:13 +0300

    CR fix

--- src/command_line.py
+++ src/command_line.py
@@ -1,12 +1,13 @@
 import argparse
 import tempfile
+import re
 from str2bool import str2bool
 
-
+from src.packagers import packager_type_codes, default_packager_type_code
 from src.providers import providers
 
 
-__parser = argparse.ArgumentParser()
+__parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
 
 __parser.add_argument('--concurrency-per-core', '-c', type=int, default=2, dest='concurrency',
                       help='Number of workers = concurrency * CPU_CORES_COUNT')
@@ -14,12 +15,17 @@ __parser.add_argument('--input-dir', '-i', type=str, default='./in',
                       help='Base directory where list files are located')
 __parser.add_argument('--output-dir', '-o', type=str, default='./out',
                       help='Base directory to place bundled zip file in')
-__parser.add_argument('--packager', '-p', type=str, default='m',
-                      help='Which packager to use (m for memory based packager or f for FS based packager)')
 __parser.add_argument('--temp-dir', '-t', type=str, default=tempfile.TemporaryDirectory().name,
                       help='Base directory to place all downloaded files before bundling. Applies only for FS based packager')
 __parser.add_argument('--strict-ssl', type=str2bool, default=True,
                       help='False disables ssl certificate checks')
+
+packager_type_helps = [
+    '{0} - use{1}'.format(code, re.sub('([A-Z])', ' \\g<0>', packager_type.__name__))
+    for code, packager_type in packager_type_codes.items()
+]
+__parser.add_argument('--packager', '-p', type=str, default=default_packager_type_code, choices=packager_type_codes.keys(),
+                      help='Which packager to use. options are:\n{0}'.format('\n'.join(packager_type_helps)))
 
 for provider in providers:
     __parser.add_argument('--{0}'.format(provider['name']),
--- src/packagers/__init__.py
+++ src/packagers/__init__.py
@@ -8,11 +8,18 @@ from .FileSystemPackager import FileSystemPackager
 from .InMemoryPackager import InMemoryPackager
 from .Packager import Packager
 
+packager_type_codes = {
+    'm': InMemoryPackager,
+    'f': FileSystemPackager,
+}
 
-def get_packager(packager_type, *, logger:Logger, output_dir: str, temp_dir: str = '', num_workers:int = 1, **kargs) -> Packager:
-    if packager_type == 'm':
+default_packager_type_code = 'm'
+
+def get_packager(packager_type_code, *, logger:Logger, output_dir: str, temp_dir: str = '', num_workers:int = 1, **kargs) -> Packager:
+    packager = packager_type_codes[packager_type_code]
+    if packager == InMemoryPackager:
         return InMemoryPackager(logger, output_dir)
-    elif packager_type == 'f':
+    elif packager == FileSystemPackager:
         return FileSystemPackager(logger, output_dir, temp_dir, num_workers)
     else:
         raise ValueError('Unknown packager type')
